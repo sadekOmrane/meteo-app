ba89b00a61435ec42eeb02f4e6d2d703d7b92a41
SADEK OMRANE <omranesadok33@gamil.com>
2024-02-02 01:43:42 +0100

    publish develop branch

--- pages/home.page.js
+++ pages/home.page.js
@@ -2,6 +2,7 @@ import { Text, StyleSheet, View } from "react-native";
 
 export default function HomePage() {
   return (
+    
     <View style={styles.container}>
       <Text style={styles.text}>Hello, world!</Text>
     </View>
