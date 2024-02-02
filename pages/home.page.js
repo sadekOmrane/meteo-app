8e770a60353fdc4e1c09fb6dd6cbb3037c43c9f0
SADEK OMRANE <omranesadok33@gamil.com>
2024-02-02 01:46:20 +0100

    home page
    design du home page

--- pages/home.page.js
+++ pages/home.page.js
@@ -1,21 +1,65 @@
-import { Text, StyleSheet, View } from "react-native";
+import { StatusBar, Text, StyleSheet, View, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
+import NavbarComponent from "../components/navbar.component";
+import Icon from "react-native-vector-icons/Ionicons";
+import HumiditeComponent from "../components/humidite.component";
 
 export default function HomePage() {
   return (
-    
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, world!</Text>
-    </View>
+    <LinearGradient
+      colors={["#08244F", "#134CB5", "#0B42AB"]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <SafeAreaView>
+        <View style={styles.container}>
+          <NavbarComponent />
+          <Image
+            source={require("../assets/sun-cloud-angled-rain.png")}
+            style={styles.image}
+          />
+          <Text style={styles.temp}>18° C <Icon name="chevron-down" size={35}/></Text>
+          <Text style={styles.txt}>Precipitations</Text>
+          <Text style={styles.rangeTemp}>Max: 31º Min: 25º</Text>
+          <HumiditeComponent />
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
   },
-  text: {
-    fontSize: 20,
+  image: {
+    marginTop: 100,
+  },
+  temp: {
+    color: "#fff",
+    fontSize: 50,
+    fontWeight: "medium",
+    marginTop: 20,
+  },
+  txt: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "normal",
+    marginTop: 5,
+  },
+  rangeTemp: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "normal",
+    marginTop: 10,
   },
 });
